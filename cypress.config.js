bc5808b8b3ab3d99a5a0a7a5d889bc6e616f0359
bruna-boneberg <bruna_boneberg@hotmail.com>
2023-08-31 21:53:21 -0300

    create test - register a new task

--- cypress.config.js
+++ cypress.config.js
@@ -7,12 +7,18 @@ module.exports = defineConfig({
     async setupNodeEvents(on, config) {
       // implement node event listeners here
       const db = await connect();
+
       on("task", {
         async deleteUser(email) {
           const users = db.collection("users");
           await users.deleteMany({ email: email });
           return null;
         },
+        async deleteTask(taskName) {
+          const tasks = db.collection("tasks")
+          await tasks.deleteMany({ name: taskName })
+          return null
+        }
       });
     },
     baseUrl: "http://localhost:3333",
